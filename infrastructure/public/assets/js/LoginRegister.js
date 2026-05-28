cd7eb218e91807cc76cd9a547474a65c1009a7db
Duy La <banvietthenaocungdc@gmail.com>
2026-05-28 15:08:08 +0000

    UI/UX cho trang đăng nhập / đăng ký tài khoản trên hệ thống

---
+++ infrastructure/public/assets/js/LoginRegister.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginSection = document.getElementById("login-section");
+  const registerSection = document.getElementById("register-section");
+  const toggleToRegister = document.getElementById("toggle-to-register");
+  const toggleToLogin = document.getElementById("toggle-to-login");
+
+  console.log("dcmm!");
+
+  // Switching to register form
+  toggleToRegister.addEventListener("click", () => {
+    console.log("Clicked to display the login section!");
+    loginSection.classList.add("hidden");
+    registerSection.classList.remove("hidden");
+    document.title = "Register - Club&Event Seeker";
+  });
+
+  // Switching back to login form
+  toggleToLogin.addEventListener("click", () => {
+    registerSection.classList.add("hidden");
+    loginSection.classList.remove("hidden");
+    document.title = "Sign In - Club&Event Seeker";
+    console.log("Clicked to display the signup section!");
+  });
+
+  // Auto-opening register tab if URL contains #register (e.g: from the nav-bar button)
+  if (window.location.hash === "#register") {
+    toggleToRegister.click();
+  }
+});
